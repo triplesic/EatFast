2ec7e490b9c7b4cd8fba8ecd06969b56d6fdef93
Triplesic <triplesic@gmail.com>
2017-02-02 17:19:07 +0700

    Initial EatFast Project

---
+++ dakduan.js
@@ -0,0 +1,18 @@
+var ApiBuilder = require('claudia-api-builder'),
+  api = new ApiBuilder();
+
+module.exports = api;
+var dbConn = null;
+
+api.get('/location', function (request) {
+  var data = [ {"name":"ป้าหมูกรอบ", "description":"ซอยสี่", "lat": 44, "lng": 33},
+               {"name":"กวงโภชนา", "description":"ซอยสี่", "lat": 23, "lng": 63} ];
+  return JSON.stringify(data);
+});
+
+api.get('/shop', function (request) {
+  var data = [ {"name":"ป้าหมูกรอบ", Menu:["ข้าวกระเพราหมูกรอบ","ข้าวราดแขนงหมูกรอบ"]},
+               {"name":"กวงโภชนา", Menu:["ผัดซิิิอิ้ว","ข้าวซอยไก่"]},
+               {"name":"ซาลอน", Menu:["ข้าวผัดพริกแกงใต้","หมูตุ๋น"]} ];
+  return JSON.stringify(data);
+});
